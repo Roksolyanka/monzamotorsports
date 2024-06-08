ddcc53967cb32891fad04d3d5691dfc6cd404685
Roksolyanka <119354842+Roksolyanka@users.noreply.github.com>
2024-06-08 01:18:29 +0000

    Deploying to gh-pages from @ Roksolyanka/monzamotorsports@59ffbbe87bf80f69e7ff58c40f21bb7c86681c0f 🚀

---
+++ index.06d20cfb.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){document.querySelector('a[href="#offers"]').addEventListener("click",(function(e){e.preventDefault(),document.querySelector("#offers").scrollIntoView({behavior:"smooth"})}))}));
+//# sourceMappingURL=index.06d20cfb.js.map
---
+++ index.1dcb438f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){document.querySelector('a[href="#offers"]').addEventListener("click",(function(e){e.preventDefault(),document.querySelector("#offers").scrollIntoView({behavior:"smooth"})}))}));
+//# sourceMappingURL=index.1dcb438f.js.map
